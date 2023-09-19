f66a847f07218e57208e6e440b12cb335d10faf1
Frida Iversen <fridabenedikte@dhcp-10-22-8-89.wlan.ntnu.no>
2023-09-19 11:18:36 +0200

    endret fxml filer

--- javafx-template/src/main/java/app/AppControllerGames.java
+++ javafx-template/src/main/java/app/AppControllerGames.java
@@ -6,16 +6,6 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
 public class AppControllerGames {
-    @FXML
-    private TextField addName, addAge;
-
-    @FXML
-    private Button AddPlayer;
-
-    private int StringToInt(String string){
-        return Integer.valueOf(string);
-    }
-
     
     
 }
