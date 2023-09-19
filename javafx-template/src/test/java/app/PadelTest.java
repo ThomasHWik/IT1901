319f9834732872a56ee708d973220190004acf62
Frida Iversen <fridabenedikte@dhcp-10-22-8-89.wlan.ntnu.no>
2023-09-19 12:07:51 +0200

    made test #7

---
+++ javafx-template/src/test/java/app/PadelTest.java
@@ -0,0 +1,23 @@
+package app;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class PadelTest {
+    @Test
+    void testPlayer() {
+
+        Player player= new Player("Jon", 23, 2);
+
+        
+        Assertions.assertTrue(player.getName() =="Jon");
+        Assertions.assertTrue(player.getAge() == 23);
+        Assertions.assertTrue(player.getGrade() == 2);
+        
+    }
+
+    
+
+
+
+}
