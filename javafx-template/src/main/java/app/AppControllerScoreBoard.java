3bd7aed097115021e232ef1b770a1dd947ce43f5
Kjartan Lyse Sørskår <kjartals@stud.ntnu.no>
2023-10-03 23:03:02 +0200

    started on the controller for the scoreboard #14

--- javafx-template/src/main/java/app/AppControllerScoreBoard.java
+++ javafx-template/src/main/java/app/AppControllerScoreBoard.java
@@ -1,11 +1,26 @@
 package app;
 
-import javafx.event.ActionEvent;
+import java.io.IOException;
+import java.util.ArrayList;
+
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
+import javafx.scene.control.TextArea;
 
 public class AppControllerScoreBoard {
-    
-    
+
+    private PlayerComparator pc;
+    private FileManager fm = new FileManager();
+    private ArrayList<Player> scorelist;
+
+    @FXML
+    private TextArea scoreboard;
+
+    public void createScoreboard() throws IOException {
+        // fm.saveScoreboard(scorelist);
+    }
+
+    public void setScoreboard(ArrayList<Player> playerlist) {
+        this.scorelist = playerlist;
+        playerlist.sort(pc);
+    }
 }
