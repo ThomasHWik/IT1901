193418c60692b5c9f18b6f853a47665da2637ad1
ThomasHWik <146004751+ThomasHWik@users.noreply.github.com>
2023-10-12 15:20:26 +0200

    small changes

--- PadelApp/core/src/main/java/PadelApp/core/PlayerComparator.java
+++ PadelApp/core/src/main/java/PadelApp/core/PlayerComparator.java
@@ -1,4 +1,4 @@
-package PadelApp.core;
+package core;
 
 import java.util.Comparator;
 
--- PadelApp/ui/src/main/java/PadelApp/ui/AppControllerGames.java
+++ PadelApp/ui/src/main/java/PadelApp/ui/AppControllerGames.java
@@ -1,4 +1,4 @@
-package PadelApp.ui;
+package ui;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
