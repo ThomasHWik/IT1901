7f9916faf998ecc9c1bbfd5ec4a330b4a3df8082
jennystr <jennystr@stud.ntnu.no>
2023-10-26 12:12:32 +0200

    created a RoundSelector class in core, #59

---
+++ PadelApp/core/src/main/java/PadelApp/core/RoundSelector.java
@@ -0,0 +1,19 @@
+package PadelApp.core;
+
+public class RoundSelector {
+    
+    private int numberOfRounds;
+
+    public RoundSelector(int numberOfRounds) {
+        this.numberOfRounds = numberOfRounds;
+    }
+
+    public int getNumberOfRounds(){
+        return numberOfRounds;
+    }
+
+    public void setNumberOfRounds(int numberOfRounds) {
+        this.numberOfRounds = numberOfRounds;
+    }
+   
+}
