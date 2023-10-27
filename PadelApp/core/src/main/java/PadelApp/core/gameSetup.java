9a6de7fa1f0e75f7c5a45ef99b1bf94442670e61
Frida Iversen <fridabenedikte@dhcp-10-22-10-6.wlan.ntnu.no>
2023-10-27 14:44:50 +0200

    finished first draft gamesetup #55

--- PadelApp/core/src/main/java/PadelApp/core/gameSetup.java
+++ PadelApp/core/src/main/java/PadelApp/core/gameSetup.java
@@ -5,41 +5,49 @@ import java.util.List;
 
 public class gameSetup {
 
-    private int singelCourt=2;
-    private int doubleCourt=1;
+    private int maxSingle=3;
+    private int maxDouble=2;
+    private int maxPlayerpairs=5;
+
+    private int singelCourt;
+    private int doubleCourt;
     private List<PlayerPair> PlayerPairslist = new ArrayList<>();
 
     public gameSetup(int doubleCourt, List<PlayerPair> playerPairslist) {
         this.PlayerPairslist = playerPairslist;
+        courts(doubleCourt);
     }
 
     private void courts(int ammount){
+    //sets up the ammount of courts needed after you choose how many double courts you want
         if(PlayerPairslist.size()<ammount*2){
             throw new IllegalArgumentException("There has to be at least 4 players per double Court");
         }
-        if(ammount>2) {
-            throw new IllegalArgumentException("We only have 2 double courts");
+        if(ammount>maxDouble) {
+            throw new IllegalArgumentException("We only have "+ maxDouble+" double courts");
         }
-        if(PlayerPairslist.size()>5){
-            throw new IllegalArgumentException("We don´t have enough cours for more than 10 palyers");
+        if(PlayerPairslist.size()>maxPlayerpairs){
+            throw new IllegalArgumentException("We don´t have enough courts for more than"+ maxPlayerpairs*2 + "players");
         }
-        if(PlayerPairslist.size()==ammount*2){
-            doubleCourt=ammount;
-            singelCourt=0;
-        }
-        else if(ammount==0){
+       if(ammount==0){
+            if(PlayerPairslist.size()>maxSingle)throw new IllegalArgumentException("There is only " + maxSingle + " avalable single courts");
+            doubleCourt=0;
             singelCourt=PlayerPairslist.size();
         }
-        else if(ammount==1){
-            
-
+        else{
+            doubleCourt=ammount;
+            singelCourt=PlayerPairslist.size() - ammount*2;
         }
     
     }
+
+    public int getSingle(){
+        return singelCourt;
+    }
+
+    public int getDouble(){
+        return doubleCourt;
+    }
     
-
-
-
-
     
 }
