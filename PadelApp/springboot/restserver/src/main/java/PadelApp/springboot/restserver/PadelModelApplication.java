28d1886ee8de52276d5dc285156cf0cf56d0280a
ThomasHWik <146004751+ThomasHWik@users.noreply.github.com>
2023-11-03 12:19:56 +0100

    feat: added Springboot app framework #76
    Co-Authored-by: Kacper Jacek Kreczko

---
+++ PadelApp/springboot/restserver/src/main/java/PadelApp/springboot/restserver/PadelModelApplication.java
@@ -0,0 +1,24 @@
+package PadelApp.springboot.restserver;
+
+import com.fasterxml.jackson.databind.Module;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import PadelApp.json.FileManagerJson;
+
+
+/**
+ * The Spring application.
+ */
+@SpringBootApplication
+public class PadelModelApplication {
+
+  @Bean
+  public Module objectMapperModule() {
+    
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(PadelModelApplication.class, args);
+  }
+}
