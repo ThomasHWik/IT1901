bef6e20c5399d9f424185e52f2b1cada703b01de
Kacper Jacek Kreczko <kacperjk@stud.ntnu.no>
2023-11-03 12:59:08 +0100

    fix: imported objectmapper

--- PadelApp/springboot/restserver/src/main/java/PadelApp/springboot/restserver/PadelModelApplication.java
+++ PadelApp/springboot/restserver/src/main/java/PadelApp/springboot/restserver/PadelModelApplication.java
@@ -1,6 +1,8 @@
 package PadelApp.springboot.restserver;
 
 import com.fasterxml.jackson.databind.Module;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
