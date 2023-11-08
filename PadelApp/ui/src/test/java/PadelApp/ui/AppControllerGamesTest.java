6c955e57a22d0d8dec0b3ed2baa8c34257c3b2f1
ThomasHWik <146004751+ThomasHWik@users.noreply.github.com>
2023-11-08 12:19:44 +0100

    fix: updated ui-tests to work with new ui-setup #90
    Co-Authored-by Frida Benedikte Iversen

--- PadelApp/ui/src/test/java/PadelApp/ui/AppControllerGamesTest.java
+++ PadelApp/ui/src/test/java/PadelApp/ui/AppControllerGamesTest.java
@@ -41,7 +41,7 @@ public class AppControllerGamesTest extends ApplicationTest {
         games.CreateGame();
     }
 
-    public void AddPlayers() {
+    public void Add4Players() {
         // Input test data
         String player1Name = "John";
         String player1Age = "25";
@@ -86,30 +86,83 @@ public class AppControllerGamesTest extends ApplicationTest {
 
     }
 
+    public void Add2Players() {
+        // Input test data
+        String player1Name = "John";
+        String player1Age = "25";
+        String Player1number = "95643241";
+
+        // Locate the text fields and button by their fx:id
+        clickOn("#addTlfNr").write(Player1number);
+        clickOn("#addName").write(player1Name);
+        clickOn("#addAge").write(player1Age);
+        clickOn("#AddPlayer");
+
+        String player2Name = "Tom";
+        String player2Age = "21";
+        String Player2number = "45464748";
+
+        // Locate the text fields and button by their fx:id
+        clickOn("#addTlfNr").write(Player2number);
+        clickOn("#addName").write(player2Name);
+        clickOn("#addAge").write(player2Age);
+        clickOn("#AddPlayer");
+        clickOn("#CreateGame");
+
+    }
+
     @Test
-    public void testAddPointstoPlayer() {
-        AddPlayers();
-
-        clickOn("#onel");
-        clickOn("#twor");
+    public void testAddPointstoPlayer4() {
+        Add4Players();
+        clickOn("#threeOne1");
         clickOn("#NewRound");
 
-        clickOn("#oner");
-        clickOn("#twor");
+        clickOn("#threeOne2");
         clickOn("#NewRound");
 
-        clickOn("#oner");
-        clickOn("#twol");
+        clickOn("#threeTwo1");
         clickOn("#NewRound");
+
+        clickOn("#threeTwo2");
+        clickOn("#NewRound");
+
+        clickOn("#threeTwo2");
         clickOn("#GoToScore");
 
         playerList = FileManagerJson.getScoreboard("currentgame").getScorelist();
 
-        assertTrue(playerList.get(0).getWins() == 1);
-        assertTrue(playerList.get(1).getWins() == 2);
-        assertTrue(playerList.get(2).getWins() == 1);
-        assertTrue(playerList.get(3).getWins() == 2);
+        assertTrue(playerList.get(0).getWins() == 2);
+        assertTrue(playerList.get(1).getWins() == 3);
+        assertTrue(playerList.get(2).getWins() == 2);
+        assertTrue(playerList.get(3).getWins() == 3);
 
     }
+
+    @Test
+    public void testAddPointstoPlayer2() {
+        Add2Players();
+        // threeone1, threeone2
+        // threetwo1, threetwo2
+        clickOn("#oneOne");
+        clickOn("#NewRound");
+
+        clickOn("#oneTwo");
+        clickOn("#NewRound");
+
+        clickOn("#oneTwo");
+        clickOn("#NewRound");
+
+        clickOn("#oneOne");
+        clickOn("#NewRound");
+
+        clickOn("#GoToScore");
+
+        playerList = FileManagerJson.getScoreboard("currentgame").getScorelist();
+
+        assertTrue(playerList.get(0).getWins() == 2);
+        assertTrue(playerList.get(1).getWins() == 2);
+
+    }
+
 
 }
