8ac6a585c81dfd2a5a1da70b6ce32ba61b3737d5
Kacper Jacek Kreczko <kacperjk@stud.ntnu.no>
2023-11-08 13:18:03 +0100

    feat: javadocs for springboot controller #98 #57

--- PadelApp/springboot/restserver/src/main/java/PadelApp/springboot/restserver/PadelModelController.java
+++ PadelApp/springboot/restserver/src/main/java/PadelApp/springboot/restserver/PadelModelController.java
@@ -17,11 +17,20 @@ import PadelApp.json.FileManagerJson;
 public class PadelModelController {
     private final PadelModelService padelModelService;
 
+    /**
+     * Constructor for PadelModelController class.
+     * 
+     * @param padelAppService an instance of PadelModelService to be injected via Autowired annotation.
+     */
     @Autowired
     public PadelModelController(PadelModelService padelAppService) {
         this.padelModelService = padelAppService;
     }
 
+    /**
+     * Endpoint for retrival of leaderboard from the PadelModelService and returns it as a ResponseEntity.
+     * @return ResponseEntity<Leaderboard> containing the leaderboard if it exists, or HttpStatus.NOT_FOUND if it doesn't.
+     */
     @GetMapping("/leaderboard")
     public ResponseEntity<Leaderboard> getLeaderboard() {
         Leaderboard leaderboard = padelModelService.getLeaderboard();
@@ -32,6 +41,13 @@ public class PadelModelController {
         }
     }
 
+    /**
+     * Endpoint for receiving a scoreboard object in json format and adding it to the leaderboard.
+     * 
+     * @param scoreboard the scoreboard object to be added to the leaderboard
+     * @return a ResponseEntity object with a success message and HTTP status code 200 if the scoreboard was processed successfully, 
+     *         or an error message and HTTP status code 500 if an error occurred while processing the scoreboard
+     */
     @PostMapping("/addScoreboard")
     public ResponseEntity<String> receiveScoreboard(@RequestBody Scoreboard scoreboard) {
         try {
@@ -44,6 +60,7 @@ public class PadelModelController {
         }
     }
 
+    /** 
     @GetMapping("/test")
     public ResponseEntity<String> testServer(){
         Leaderboard leaderboard = new Leaderboard();
@@ -58,7 +75,14 @@ public class PadelModelController {
             return new ResponseEntity<>("Failed json serialization", HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
+    */
 
+    /**
+     * Endpoint for initializing the PadelModelService.
+     * 
+     * @return a ResponseEntity object with a success message and HTTP status code 200 if the PadelModelService was initialized successfully, 
+     *         or an error message and HTTP status code 500 if an error occurred while initializing the PadelModelService
+     */
     @GetMapping("/initialize")
     public ResponseEntity<String> initialize() {
         padelModelService.initialize();
