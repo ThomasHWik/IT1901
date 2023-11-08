b02d4e618ec40ca659e743b50f7dc8f7a5a398b5
Kacper Jacek Kreczko <kacperjk@stud.ntnu.no>
2023-11-08 13:00:12 +0100

    feat: javadocs for appControllerScoreboard class #57 #98

--- PadelApp/ui/src/main/java/PadelApp/ui/AppControllerScoreBoard.java
+++ PadelApp/ui/src/main/java/PadelApp/ui/AppControllerScoreBoard.java
@@ -11,6 +11,12 @@ import PadelApp.core.Scoreboard;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
 
+/**
+ * This class represents the controller for the scoreboard view of the PadelApp.
+ * It initializes the leaderboard and populates the table with the top 10 players.
+ * It also provides methods to create a leaderboard and set the scoreboard for a list of players.
+ * The class uses FileManagerJson to read and write data to JSON files.
+ */
 public class AppControllerScoreBoard {
 
     private Leaderboard leaderboard;
@@ -22,6 +28,12 @@ public class AppControllerScoreBoard {
     @FXML
     ListView<Integer> wins;
 
+    /**
+     * Initializes the AppControllerScoreBoard by loading the leaderboard from a JSON file using FileManagerJson.getLeaderboard method.
+     * If the leaderboard is null, a new Leaderboard object is created.
+     * Calls createLeaderboard method to create the leaderboard UI and populateTable method to populate the table with data.
+     * @throws IOException if there is an error reading the JSON file.
+     */
     @FXML
     public void initialize() throws IOException {
         leaderboard = FileManagerJson.getLeaderboard("Leaderboard");
@@ -32,16 +44,29 @@ public class AppControllerScoreBoard {
         populateTable();
     }
 
+    /**
+     * Adds the current game's scoreboard to the leaderboard, sorts it, and saves it to a file.
+     * @throws IOException if there is an error reading or writing the file.
+     */
     public void createLeaderboard() throws IOException {
         leaderboard.addScoreboard(FileManagerJson.getScoreboard("currentgame"));
         leaderboard.sortLeaderboard();
         FileManagerJson.saveScoreboard(leaderboard);
     }
 
+    /**
+     * Sets the scoreboard with the given list of players.
+     * 
+     * @param playerlist the list of players to set the scoreboard with
+     */
     public void setScoreboard(List<Player> playerlist) {
         scoreboard = new Scoreboard((ArrayList<Player>) playerlist);
     }
 
+    /**
+     * Populates the table with the top 10 players in the leaderboard.
+     * Clears the name and wins items before adding the new data.
+     */
     private void populateTable() {
         name.getItems().clear();
         wins.getItems().clear();
