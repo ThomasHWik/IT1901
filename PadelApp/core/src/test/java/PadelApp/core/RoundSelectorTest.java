0f81db93adf8999f7956913bba2f358f84e8ba03
ThomasHWik <146004751+ThomasHWik@users.noreply.github.com>
2023-11-13 22:28:58 +0100

    feat: added tests of RoundSelector class #113
    Co-Authored-by: Jenny Straumbotn

---
+++ PadelApp/core/src/test/java/PadelApp/core/RoundSelectorTest.java
@@ -0,0 +1,39 @@
+package PadelApp.core;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class RoundSelectorTest {
+    RoundSelector roundSelector;
+
+    @BeforeEach
+    void setUp() {
+        // Create a new RoundSelector with 5 number of rounds before each test
+        roundSelector = new RoundSelector(5);
+    }
+
+    @Test
+    void testRoundSelector() {
+        assertEquals(5, roundSelector.getNumberOfRounds());
+    }
+
+    @Test
+    void testGetNumberOfRounds() {
+        assertEquals(5, roundSelector.getNumberOfRounds());
+    }
+
+    @Test
+    void testSetNumberOfRounds() {
+        roundSelector.setNumberOfRounds(7);
+        assertEquals(7, roundSelector.getNumberOfRounds());
+
+        // Should throw an IllegalArgumentException when trying to set number
+        // of rounds to 0, a negative number or higher than 10
+        assertThrows(IllegalArgumentException.class, () -> roundSelector.setNumberOfRounds(0));
+        assertThrows(IllegalArgumentException.class, () -> roundSelector.setNumberOfRounds(-3));
+        assertThrows(IllegalArgumentException.class, () -> roundSelector.setNumberOfRounds(11));
+    }
+
+}
