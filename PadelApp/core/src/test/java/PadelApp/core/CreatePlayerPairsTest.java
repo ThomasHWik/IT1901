740b014777282fdf45d3886558ef015349577b35
ThomasHWik <146004751+ThomasHWik@users.noreply.github.com>
2023-11-13 23:43:23 +0100

    feat: added tests of new CreatePlayerPairs methods #115
    Co-Authored-by: Frida Benedikte Iversen

--- PadelApp/core/src/test/java/PadelApp/core/CreatePlayerPairsTest.java
+++ PadelApp/core/src/test/java/PadelApp/core/CreatePlayerPairsTest.java
@@ -8,13 +8,13 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-
 public class CreatePlayerPairsTest {
     private Player player1;
     private Player player2;
     private Player player3;
     private Player player4;
     private List<Player> players;
+    private CreatePlayerPairs createPlayerPairs;
 
     @BeforeEach
     void setUp() {
@@ -30,19 +30,20 @@ public class CreatePlayerPairsTest {
         players.add(player2);
         players.add(player3);
         players.add(player4);
+
+        createPlayerPairs = new CreatePlayerPairs(players);
     }
 
     @Test
-    // Tests the getPlayerPairs method, as well as whether the createPlayerPairs methods work
+    // Tests the getPlayerPairs method, as well as whether the createPlayerPairs
+    // methods work
     void testGetPlayerPairs() {
-        CreatePlayerPairs createPlayerPairs = new CreatePlayerPairs(players);
         List<PlayerPair> expectedPairs = new ArrayList<>();
         PlayerPair pair1 = new PlayerPair(player1, player2);
         PlayerPair pair2 = new PlayerPair(player3, player4);
         expectedPairs.add(pair1);
         expectedPairs.add(pair2);
         List<PlayerPair> actualPairs = createPlayerPairs.getPlayerPairs();
-
 
         assertEquals(expectedPairs.get(0).getPlayer1(), actualPairs.get(0).getPlayer1());
         assertEquals(expectedPairs.get(0).getPlayer2(), actualPairs.get(0).getPlayer2());
@@ -57,7 +58,34 @@ public class CreatePlayerPairsTest {
         oddNumberedPlayers.add(player1);
         oddNumberedPlayers.add(player2);
         oddNumberedPlayers.add(player3);
-        // Should throw an IllegalArgument Exception when attempting to create pairs from an odd number of players
+        // Should throw an IllegalArgument Exception when attempting to create pairs
+        // from an odd number of players
         assertThrows(IllegalArgumentException.class, () -> new CreatePlayerPairs(oddNumberedPlayers));
+    }
+
+    @Test
+    void testRemakePlayersOrder() {
+        createPlayerPairs.remakePlayersOrder();
+
+        List<PlayerPair> expectedPairs = new ArrayList<>();
+        PlayerPair pair1 = new PlayerPair(player4, player1);
+        PlayerPair pair2 = new PlayerPair(player2, player3);
+        expectedPairs.add(pair1);
+        expectedPairs.add(pair2);
+        List<PlayerPair> actualPairs = createPlayerPairs.getPlayerPairs();
+
+        assertEquals(expectedPairs.get(0).getPlayer1(), actualPairs.get(0).getPlayer1());
+        assertEquals(expectedPairs.get(0).getPlayer2(), actualPairs.get(0).getPlayer2());
+
+        assertEquals(expectedPairs.get(1).getPlayer1(), actualPairs.get(1).getPlayer1());
+        assertEquals(expectedPairs.get(1).getPlayer2(), actualPairs.get(1).getPlayer2());
+
+    }
+
+    @Test
+    void testToString() {
+        List<PlayerPair> PlayerPairslist = createPlayerPairs.getPlayerPairs();
+        List<Player> playerList = createPlayerPairs.getPlayerlist();
+        assertEquals("CreatePlayerPairs [PlayerPairslist=" + PlayerPairslist + ", playerList=" + playerList + "]", createPlayerPairs.toString());
     }
 }
